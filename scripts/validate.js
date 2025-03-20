fbffc6f7543e8f8888b98fd6ae5610821e5d3733
Marcus Tillmanns <marcus.tillmanns@qt.io>
2025-03-20 13:29:13 +0100

    Add icon checking

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,7 +1,9 @@
 const Ajv = require("ajv/")
 const path = require("path")
 const fs = require("fs")
+const fsp = require("fs/promises")
 const styleTextOrg = require('node:util').styleText
+const probe = require('probe-image-size');
 
 function styleText(color, text) {
     // Github actions don't have a real tty, so styleText will normally output monochrome text.
@@ -30,24 +32,11 @@ const validatePack = ajv.compile(packSchema)
 
 const registryFolder = path.join(rootFolder, "registry")
 
-function enumerateExtensions(directory) {
-    return fs.readdirSync(directory).map((folder) => {
-        const extpath = path.join(directory, folder, "extension.json")
-        const packpath = path.join(directory, folder, "pack.json")
-        if (fs.existsSync(extpath)) {
-            return extpath
-        } else if (fs.existsSync(packpath)) {
-            return packpath
-        }
-        throw new Error(`No extension or pack found in ${folder}`)
-    })
-}
-
-function checkDirectoryContents(directory) {
-    // Make sure that each extension folder only contains the allowed files
+async function checkDirectoryContents(directory) {
+    console.log(`Checking ${styleText('green', path.relative(rootFolder, directory))}`)
     const allowedFiles = ["extension.json", "pack.json", "icon.png", "icon@2x.png"]
 
-    const files = fs.readdirSync(directory)
+    const files = await fsp.readdir(directory)
     if (!files.includes("extension.json") && !files.includes("pack.json")) {
         console.error(`No extension or pack found in ${styleText('red', path.relative(rootFolder, directory))}`)
         process.exit(1)
@@ -62,24 +51,93 @@ function checkDirectoryContents(directory) {
     }
 }
 
-function checkExtensionDirectoryContents(directory) {
-    const extensions = fs.readdirSync(directory)
-    for (const extension of extensions) {
-        checkDirectoryContents(path.join(directory, extension))
+async function checkFileSize(file, maxSize) {
+    const fileSize = (await fsp.stat(file)).size
+    if (fileSize > maxSize) {
+        console.error(`File ${styleText("green", path.relative(rootFolder, file))} is too large: ${styleText("red", `${fileSize} bytes`)}`)
+        process.exit(1)
     }
-
-    console.log(`All extensions in ${styleText("green", path.relative(rootFolder, directory))} have the correct files`)
 }
 
-function validate(validator, ext) {
-    const res = validator(JSON.parse(fs.readFileSync(ext)))
-    if (!res) {
-        console.error(`Schema ${styleText("red", path.relative(registryFolder, ext))}:`, styleText("red", "failed"))
-        console.error(validator.errors)
+async function checkImageSize(image, expectedSize) {
+    const iconInfo = await probe(fs.createReadStream(image));
+    if (iconInfo.mime !== 'image/png' || iconInfo.type !== 'png') {
+        console.error(`Icon ${styleText("green", path.relative(rootFolder, icon))} is not a PNG file`)
         process.exit(1)
     }
 
-    console.log(`Schema ${styleText("green", path.relative(registryFolder, ext))}:`, styleText("green", "Ok"))
+    if (iconInfo.width !== expectedSize || iconInfo.height !== expectedSize) {
+        console.error(`Icon ${styleText("green", path.relative(rootFolder, image))} has the wrong size: ${styleText("red", `${iconInfo.width}x${iconInfo.height}`)}`)
+        process.exit(1)
+    }
+}
+
+async function checkIcon(icon, expectedSize) {
+    await Promise.all([
+        checkFileSize(icon, 100 * 1024),
+        checkImageSize(icon, expectedSize)
+    ])
+}
+
+async function checkIcons(directory) {
+    const files = await fsp.readdir(directory)
+    const icons = files.filter((file) => file === "icon.png" || file === "icon@2x.png")
+    if (icons.length === 0) {
+        return
+    }
+    if (icons.length === 1) {
+        console.error(`Only one icon found in ${styleText("green", path.relative(rootFolder, directory))}`)
+        process.exit(1)
+    }
+
+    await Promise.all([
+        checkIcon(path.join(directory, "icon.png"), 16),
+        checkIcon(path.join(directory, "icon@2x.png"), 32)
+    ])
+}
+
+async function checkJsons(directory) {
+    try {
+        const extensionData = JSON.parse(await fsp.readFile(path.join(directory, "extension.json")))
+        validateExtensionData(extensionData)
+        validate(validateExtension, extensionData, path.join(directory, "extension.json"))
+    } catch (e) {
+        if (e.code !== "ENOENT") {
+            throw e
+        }
+    }
+
+    try {
+        const packData = JSON.parse(await fsp.readFile(path.join(directory, "pack.json")))
+        validate(validatePack, packData, path.join(directory, "pack.json"))
+    } catch (e) {
+        if (e.code !== "ENOENT") {
+            throw e
+        }
+    }
+}
+
+async function checkExtensionDirectoryContents(directory) {
+    const checks = (await fsp.readdir(directory)).map((extension) => {
+        return [
+            checkJsons(path.join(directory, extension)),
+            checkDirectoryContents(path.join(directory, extension)),
+            checkIcons(path.join(directory, extension))
+        ]
+    }).flat()
+
+    await Promise.all(checks)
+
+    console.log(`All extensions in ${styleText("green", path.relative(rootFolder, directory))} are ${styleText('green', 'OK')}`)
+}
+
+function validate(validator, extensionData, p) {
+    const res = validator(extensionData)
+    if (!res) {
+        console.error(`Schema ${styleText("red", path.relative(registryFolder, p))}:`, styleText("red", "failed"))
+        console.error(validator.errors)
+        process.exit(1)
+    }
 }
 
 function validateExtensionData(ext) {
@@ -111,17 +169,6 @@ function validateExtensionData(ext) {
             process.exit(1)
         }
     }
-    const id = `${ext.info.vendor_id}.${ext.info.id}`
-    console.log(`Extension data ${styleText("green", id)}:`, styleText("green", "Ok"))
 }
 
 checkExtensionDirectoryContents(registryFolder)
-
-enumerateExtensions(registryFolder).forEach((ext) => {
-    if (ext.endsWith('extension.json')) {
-        validate(validateExtension, ext)
-        validateExtensionData(JSON.parse(fs.readFileSync(ext)))
-    }
-    if (ext.endsWith('pack.json'))
-        validate(validatePack, ext)
-})
